dd3152bc08860db6b49cf1bb075d722f5d67bb13
Michael Campusano <michael.campusano@hotmail.com>
2024-04-18 17:19:49 -0400

    Adding "11.2 - find and findIndex Exercises"

---
+++ 11.2.2 - find and findIndex Exercises/app.js
@@ -0,0 +1,18 @@
+// findUserByUsername Function
+
+function findUserByUsername(usersArray, username) {
+    return usersArray.find(function(user){
+      return user.username === username;
+    })
+  }
+
+// removeUser Function
+
+function removeUser(usersArray, username) {
+    let foundIndex = usersArray.findIndex(function(user){
+      return user.username === username;
+    })
+    if(foundIndex === -1) return;
+  
+    return usersArray.splice(foundIndex,1)[0];
+  }
